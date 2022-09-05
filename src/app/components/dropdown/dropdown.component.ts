f65f65850e3fafdd8d13e6baa875901ec1aa5274
natan danilo <natan.danilo@gmail.com>
2022-09-05 01:20:27 -0200

    Correção de bug no fechar menu

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DropdownComponent implements OnInit {
 
-  constructor() { }
+  @Input() type: 'menu_site' | 'menu_fechamendo' = 'menu_site';
+
+  // CASO SEJA UM MENU DE FECHAMENTO
+  @Input() cartLength: number = 0;
+  @Input() finalPrice: number = 0;
+  @Input() userPresent: boolean = false;
+  @Input() goCart?: Function;
+  @Input() goPageRegistration?: Function;
+  @Input() currentPage: any;
+
+
+  constructor(
+    public nav: Router
+  ) { }
 
   ngOnInit() {
   }
