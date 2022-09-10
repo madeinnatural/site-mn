01c42db95d50ba2b7c3bf83c57e0250ce8b14abc
natan danilo <natan.danilo@gmail.com>
2022-09-10 20:41:35 -0200

    Ajuste do head

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://api.madeinnatural.com.br/api/',
-  apiUrl: 'https://api.madeinnatural.com.br/api/'
+  baseUrl: 'http://localhost:3000/api/',
+  apiUrl: 'http://localhost:3000/api/',
 };
 
 /*
