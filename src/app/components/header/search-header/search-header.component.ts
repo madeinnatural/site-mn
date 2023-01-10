467609e6fa78564a9a9a26e1b006aaec0c12c5ca
Natã <natan.danilo@gmail.com>
2023-01-10 15:02:51 -0300

    bug[:header] Correção de performace

--- src/app/components/header/search-header/search-header.component.ts
+++ src/app/components/header/search-header/search-header.component.ts
@@ -1,3 +1,4 @@
+import { Subject, debounceTime } from 'rxjs';
 import { GlobalEventService } from '../../../core/services/global.service';
 import { Router } from '@angular/router';
 import { Component, OnInit, EventEmitter, Input } from '@angular/core';
@@ -7,25 +8,31 @@ import { Component, OnInit, EventEmitter, Input } from '@angular/core';
   templateUrl: './search-header.component.html',
   styleUrls: ['./search-header.component.scss']
 })
-export class SearchHeaderComponent implements OnInit {
+export class SearchHeaderComponent {
 
   query: string = '';
   buscar_header = new EventEmitter<string>();
 
   @Input() type: 'mobile' | 'desktop' = 'desktop';
 
+  private searchSubject: Subject<string> = new Subject<string>();
+
   constructor(
     public router: Router,
     public globalEvents: GlobalEventService,
-  ) { }
-
-  ngOnInit() {}
+  ) {
+    this.searchSubject
+      .pipe(debounceTime(500))
+      .subscribe(query => {
+        this.query = query;
+        this.bucarProduto();
+      });
+  }
 
   monteQuery(event: KeyboardEvent){
-    if (this.query.length > 3 || event.key == 'Enter') {
-      setTimeout(() => {
-        this.bucarProduto();
-      },1000);
+    this.query = (event.target as HTMLInputElement).value;
+    if (this.query.length >= 3 || event.key == 'Enter') {
+      this.searchSubject.next(this.query);
     }
   }
 
