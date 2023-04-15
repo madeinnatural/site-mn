ab3271670d7dc453ed11c498f57f57a4702f02e9
Natã <natan.danilo@gmail.com>
2023-04-15 09:48:41 -0300

    refactor: remove tudo que não é usado em tabela

---
+++ src/app/components/table/imports.ts
@@ -0,0 +1,3 @@
+export * from './../../states-handler/store/filter.store';
+export * from './../../states-handler/store/product-showcase.store';
+export * from './../../core/domain/model/product/product';
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,10 +1,11 @@
-import { Observable } from 'rxjs';
-import { ProductsDisplay } from './../../core/model/interfaces/Product';
 import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Input, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
-import { Category, setCurrentLimit } from 'src/app/states-handler/store/filter.store';
-import { Showcase } from 'src/app/states-handler/store/product-showcase.store';
-import { ProductModel } from 'src/app/core/domain/model/product/product';
+import {
+  setCurrentLimit,
+  Showcase,
+  ProductModel
+} from './imports';
 
 @Component({
   selector: 'mn-table',
@@ -21,19 +22,20 @@ export class TableComponent implements OnInit {
   @Output() itemAdd = new EventEmitter<ProductModel>();
   @Output() showAll = new EventEmitter<boolean>();
 
+  innerWidth: number = 0;
+  @HostListener('window:resize', ['$event'])
+  productShowcase = this.store.pipe(select('productShowcase'));
+  provider$ = this.store.pipe(select('provider'));
+  constructor(
+    private store: Store<any>
+  ) {}
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
   }
 
-  innerWidth: number = 0;
-  @HostListener('window:resize', ['$event'])
   onResize() {
     this.innerWidth = window.innerWidth;
-  }
-
-  formatCategory(category: Category[]) {
-    return category.map((cat: Category) => cat.name).join(' | ')
   }
 
   removeItem(product: ProductModel) {
@@ -42,13 +44,6 @@ export class TableComponent implements OnInit {
 
   addItem(product: ProductModel) {
     this.itemAdd.emit(product);
-  }
-
-  productShowcase = this.store.pipe(select('productShowcase'));
-  provider$ = this.store.pipe(select('provider'));
-  constructor(
-    private store: Store<any>
-  ) {
   }
 
   showProductsAll() {
