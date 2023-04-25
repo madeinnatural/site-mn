a23aaf385a8ef2e085e565bdcbcd2052f072b66b
Natã <natan.danilo@gmail.com>
2023-04-25 18:41:00 -0300

    fix: corrige variavel de ambiente faltante

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  PATH_ACCESS_TOKEN: 'accessToken',
   baseUrl: 'https://api.madeinnatural.com.br/api/',
   apiUrl: 'https://api.madeinnatural.com.br/api/'
 };
